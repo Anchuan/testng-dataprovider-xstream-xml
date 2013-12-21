00a17e01e9ac02a9b9475ed07846b6327e4357c2
Jon Austen <djangofan@gmail.com>
2013-12-20 20:07:17 -0800

    Finished first version of dataprovider.

--- src/main/java/qa/dataprovider/def/TestArguments.java
+++ src/main/java/qa/dataprovider/def/TestArguments.java
@@ -10,7 +10,7 @@ import com.thoughtworks.xstream.annotations.XStreamAlias;
 @XStreamAlias("args")
 public class TestArguments {
 
-  private List<ArgObject> argsWrapper = new ArrayList<ArgObject>();
+	private List<ArgObject> argsWrapper = new ArrayList<ArgObject>();
 
 	public TestArguments() {
 		System.out.println("New TestArguments object with default required arguments.");
@@ -44,7 +44,7 @@ public class TestArguments {
 	}
 
 	public void reset() {
-    argsWrapper = new ArrayList<ArgObject>();
+		//argsWrapper = new ArrayList<ArgObject>();
 		setTestArguments( new ArrayList<List<String>>() );
 		setEnabled( "false" );
 		setTestName( "default" );
@@ -63,7 +63,7 @@ public class TestArguments {
 	}
 
 	@Override
-  public String toString() {
+	public String toString() {
 		String[] joined = new String[argsWrapper.size()];
 		int count = 0;
 		for ( ArgObject x : argsWrapper ) {
@@ -75,14 +75,14 @@ public class TestArguments {
 			count++;
 		}
 		return "[" + StringUtils.join( joined, ", " ) + "]";
-  }
+	}
 
 	/* Helpers to get values from test arguments. */
 
 	public Boolean getEnabled() {
 		ArgObject val = argsWrapper.get(0);
 		if ( val.getType().equalsIgnoreCase("java.lang.Boolean") || val.getType().equalsIgnoreCase("java.lang.boolean") ) {
-		    return Boolean.valueOf( val.getVal() );
+			return Boolean.valueOf( val.getVal() );
 		} else {
 			throw new IllegalArgumentException("The variable 'enabled' must be a string.");
 		}
@@ -91,7 +91,7 @@ public class TestArguments {
 	public String getTestName() {
 		ArgObject val = argsWrapper.get(1);
 		if ( val.getType().equalsIgnoreCase("java.lang.String") ) {
-		    return val.getVal();
+			return val.getVal();
 		} else {
 			throw new IllegalArgumentException("The variable '" + val.getKey() + "' must be a " + val.getType() + " for value '" + val.getVal() + "'.");
 		}
@@ -100,31 +100,31 @@ public class TestArguments {
 	public String getEnvironment() {
 		ArgObject val = argsWrapper.get(2);
 		if ( val.getType().equalsIgnoreCase("java.lang.String") ) {
-		    return val.getVal();
+			return val.getVal();
 		} else {
 			throw new IllegalArgumentException("The variable '" + val.getKey() + "' must be a " + val.getType() + " for value '" + val.getVal() + "'.");
 		}
 	}
 
-    public String getTestLocale() {
-    	ArgObject val = argsWrapper.get(3);
+	public String getTestLocale() {
+		ArgObject val = argsWrapper.get(3);
 		if ( val.getType().equalsIgnoreCase("java.lang.String") ) {
-		    return val.getVal();
+			return val.getVal();
 		} else {
 			throw new IllegalArgumentException("The variable '" + val.getKey() + "' must be a " + val.getType() + " for value '" + val.getVal() + "'.");
 		}
-  }
+	}
 
 	public String getBrowser() {
 		ArgObject val = argsWrapper.get(4);
 		if ( val.getType().equalsIgnoreCase("java.lang.String") ) {
-		    return val.getVal();
+			return val.getVal();
 		} else {
 			throw new IllegalArgumentException("The variable '" + val.getKey() + "' must be a " + val.getType() + " for value '" + val.getVal() + "'.");
 		}
-    }
+	}
 
-	/* Helpers to set test argument values */
+	/* Helpers to set test required argument values */
 
 	public void setEnabled( String enabled ) {
 		String en;
@@ -136,50 +136,55 @@ public class TestArguments {
 			throw new IllegalArgumentException("The variable '" + enabled + "' must be a valid Boolean value.");
 		}
 		ArgObject val = new ArgObject( "enabled", "java.lang.Boolean", en );
-		if ( argsWrapper.size() < 1 ) {
-			argsWrapper.add( 0, val );
-		} else {
+		try {
 			argsWrapper.set( 0, val );
+		} catch ( IndexOutOfBoundsException iobe ) {
+			//iobe.printStackTrace();
+			argsWrapper.add( 0, val );
 		}
 		System.out.println("Set '" + val.getKey() + "' value at index 0 to '" + val.getVal() + "' with type of '" + val.getType() + "'." );
 	}
 
 	public void setTestName( String testName ) {
 		ArgObject val = new ArgObject( "testname", "java.lang.String", testName );
-		if ( argsWrapper.size() < 2 ) {
-		    argsWrapper.add( 1, val );
-		} else {
+		try {
 			argsWrapper.set( 1, val );
+		} catch ( IndexOutOfBoundsException iobe ) {
+			//iobe.printStackTrace();
+			argsWrapper.add( 1, val );
 		}
 		System.out.println("Set '" + val.getKey() + "' value at index 1 to '" + val.getVal() + "' with type of '" + val.getType() + "'." );
-  }
+	}
 
 	public void setEnvironment( String env ) {
 		ArgObject val = new ArgObject( "environment", "java.lang.String", env );
-		if ( argsWrapper.size() < 3 ) {
-		    argsWrapper.add( 2, val );
-		} else {
+		try {
 			argsWrapper.set( 2, val );
+		} catch ( IndexOutOfBoundsException iobe ) {
+			//iobe.printStackTrace();
+			argsWrapper.add( 2, val );
 		}
 		System.out.println("Set '" + val.getKey() + "' value at index 2 to '" + val.getVal() + "' with type of '" + val.getType() + "'." );
 	}
 
 	public void setTestLocale( String testLocale ) {
 		ArgObject val = new ArgObject( "testlocale", "java.lang.String", testLocale );
-		if ( argsWrapper.size() < 4 ) {
-		    argsWrapper.add( 3, val );
-		} else {
+		try {
 			argsWrapper.set( 3, val );
+		} catch ( IndexOutOfBoundsException iobe ) {
+			//iobe.printStackTrace();
+			argsWrapper.add( 3, val );
 		}
 		System.out.println("Set '" + val.getKey() + "' value at index 3 to '" + val.getVal() + "' with type of '" + val.getType() + "'." );
 	}
 
 	public void setBrowser( String browser ) {
 		ArgObject val = new ArgObject( "browser", "java.lang.String", browser );
-		if ( argsWrapper.size() < 5 ) {
-		    argsWrapper.add( 4, val );
-		} else {
+		try {
 			argsWrapper.set( 4, val );
+		} catch ( IndexOutOfBoundsException iobe ) {
+			//iobe.printStackTrace();
+			argsWrapper.add( 4, val );
 		}
 		System.out.println("Set '" + val.getKey() + "' value at index 4 to '" + val.getVal() + "' with type of '" + val.getType() + "'." );
 	}
@@ -197,6 +202,22 @@ public class TestArguments {
 		// if no error, go ahead and add it at the end
 		argsWrapper.add( this.testSize(),  val );
 		System.out.println("Test arguments size is now: " + this.testSize() );
+	}
+
+	public void setArgument( ArgObject argObject ) {
+		System.out.println("Adding optional test property named '" + argObject.getKey() + "' with value of '" + argObject.getVal() + "' and with type of '" + argObject.getType() + "'." );
+		boolean addValue = true;
+		int count = 0;
+		for ( ArgObject arg : argsWrapper ) {
+			if ( arg.getKey().equalsIgnoreCase( argObject.getKey() ) ) {
+				argsWrapper.set( count,  argObject );
+				addValue = false;
+			}
+			count++;
+		}
+		if ( addValue ) argsWrapper.add( this.testSize(),  argObject );
+		System.out.println("Test '" + argsWrapper.get(1).getVal() + "' now has " + this.testSize() + " arguments." );
+		
 	}
 
 }
--- src/main/java/qa/dataprovider/def/TestConverter.java
+++ src/main/java/qa/dataprovider/def/TestConverter.java
@@ -8,40 +8,39 @@ import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 
 public class TestConverter implements Converter {
 
-  @SuppressWarnings("unchecked")
-  @Override
-  public boolean canConvert(Class type) {
-    return type.isAssignableFrom(TestRow.class);
-  }
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean canConvert(Class type) {
+		return type.isAssignableFrom(TestRow.class);
+	}
 
-  @Override
-  public void marshal(Object source, HierarchicalStreamWriter writer, MarshallingContext context) {
-    TestRow test = (TestRow)source;
-    for (TestArguments arg : test.getArguments()) {
-      for (ArgObject val : arg.getAllTestArguments()) {
-        writer.startNode(val.getKey());
-        writer.addAttribute("type", val.getType());
-        writer.setValue(val.getVal());
-        writer.endNode();
+	@Override
+	public void marshal( Object source, HierarchicalStreamWriter writer,	MarshallingContext context ) {
+		TestRow test = (TestRow)source;
+		for ( TestArguments arg : test.getArguments() ) {
+			for ( ArgObject val : arg.getAllTestArguments() ) {
+				writer.startNode( val.getKey() );
+				writer.addAttribute( "type", val.getType() );
+				writer.setValue(val.getVal());
+				writer.endNode();
+			}
+		}
+	}
 
-      }
-    }
-  }
-
-  @Override
-  public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) {
-    TestArguments testargs = new TestArguments();
-    while (reader.hasMoreChildren()) {
-      reader.moveDown();
-      String typeval = reader.getAttribute("type");
-      if (typeval.isEmpty()) {
-        typeval = "null";
-      }
-      testargs.getAllTestArguments().add(new ArgObject(reader.getNodeName(), typeval, reader.getValue()));
-      reader.moveUp();
-    }
-    TestRow result = new TestRow(testargs);
-    return result;
-  }
+	@Override
+	public Object unmarshal( HierarchicalStreamReader reader, UnmarshallingContext context ) {
+		TestArguments testargs = new TestArguments();
+		while ( reader.hasMoreChildren() ) {
+			reader.moveDown();
+			String typeval = reader.getAttribute("type");
+			if ( typeval.isEmpty() ) {
+				typeval = "null";
+			}
+			testargs.setArgument( new ArgObject( reader.getNodeName(), typeval, reader.getValue() ) );
+			reader.moveUp();
+		}
+		TestRow result = new TestRow( testargs );
+		return result;
+	}
 
 }
